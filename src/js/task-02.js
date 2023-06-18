d0422bcdc7f4b27c5129b69e58a142c97bd470e7
Фуревич Андрій <a.furevych@gmail.com>
2023-06-18 21:21:24 +0300

    change code task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,5 +8,15 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
-const listEl = ingredients.map((el) => `<li class='item'>${el}</li>`).join("");
-list.innerHTML = listEl;
+//const listEl = ingredients.map((el) => `<li class='item'>${el}</li>`).join("");
+//list.innerHTML = listEl;
+
+const listElements = ingredients.map((el) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.textContent = el;
+
+  return listEl;
+});
+
+list.append(...listElements);
